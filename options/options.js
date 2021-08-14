544c92ce24f6ac4c87b7c9a8d30b11e48a8dda43
Crowdbotics <team@crowdbotics.com>
2021-08-14 12:56:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lively_silence_29805Identifier",
+  url: "https://lively_silence_29805Identifier.botics.co",
+  api: "https://lively_silence_29805Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
